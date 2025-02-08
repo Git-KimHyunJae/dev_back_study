f821f70d9f099759d646f36a6bd8ae0c04baab88
김현재 <namro7633@gmail.com>
2025-02-08 19:27:26 +0900

    init commit

---
+++ src/main/java/com/mis/dev_back_study/DevBackStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mis.dev_back_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBackStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBackStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mis/dev_back_study/DevBackStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mis.dev_back_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBackStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
