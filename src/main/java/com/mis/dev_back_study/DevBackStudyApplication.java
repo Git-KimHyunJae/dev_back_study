899d6f7908a60ca9f4d53deff97306c868bb2f7f
김현재 <namro7633@gmail.com>
2025-02-09 17:55:45 +0900

    123123

--- src/main/java/com/mis/dev_back_study/DevBackStudyApplication.java
+++ src/main/java/com/mis/dev_back_study/DevBackStudyApplication.java
@@ -8,6 +8,7 @@ public class DevBackStudyApplication {
 
 	public static void main(String[] args) {
 		System.out.println("되냐??");
+		System.out.println("12312312");
 		SpringApplication.run(DevBackStudyApplication.class, args);
 	}
 
