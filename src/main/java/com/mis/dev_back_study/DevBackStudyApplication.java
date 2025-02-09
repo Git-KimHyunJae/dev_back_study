71ad8887565524ddfc42e78e3ff67290302c4ea9
김현재 <namro7633@gmail.com>
2025-02-09 17:36:10 +0900

    여기서는?

--- src/main/java/com/mis/dev_back_study/DevBackStudyApplication.java
+++ src/main/java/com/mis/dev_back_study/DevBackStudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevBackStudyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("되냐??");
 		SpringApplication.run(DevBackStudyApplication.class, args);
 	}
 
